eace2574985ab3e92521b0abd452da762554d5ba
EmilieDeOliveira <ems.deoliveira@gmail.com>
2023-11-09 10:15:46 +0100

    feat(web-app): add next.config

---
+++ web-app/app/pages/homePage/homePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./homePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <h1>My Bedtime Story</h1>
+    </div>
+
+  )
+}
+
+export default HomePage;
