a1ac7f2d1cec306741dbccaaab11dd4d802972b7
PhilSad <philippe.henri.saade@gmail.com>
2023-11-21 19:31:26 +0100

    deployed firebase function

--- back/functions/main.py
+++ back/functions/main.py
@@ -1,21 +1,37 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
-
-from firebase_functions import db_fn
-from firebase_admin import initialize_app
+from firebase_functions import firestore_fn
+from firebase_functions.firestore_fn import Event, DocumentSnapshot
+from firebase_admin import initialize_app, firestore
 from gen_story import generate_story_with_images_desc
 
 initialize_app()
 
 
-@db_fn.on_value_created(reference="/Users/{user_id}/Stories/{story_id}")
-def function_generate_story(req):
-    user_id = req.path_params["user_id"]
-    story_id = req.path_params["story_id"]
-    story_idea = req.data["story_idea"]
-    hero_name = req.data["hero_name"]
-    language = req.data["language"]
+
+def add_dummy_image_to_story(story):
+    for chapter in story:
+        for paragraph in chapter['paragraphs']:
+            paragraph['image_url'] = "https://firebasestorage.googleapis.com/v0/b/mystorytime-e88bd.appspot.com/o/test%2FComfyUI_00186_.png?alt=media"
+    return story
+
+
+@firestore_fn.on_document_created(document="/Users/{user_id}/Stories/{story_id}", timeout_sec=300)
+def function_generate_story(event: Event[DocumentSnapshot]):
+    print('>>>>>>>>>>>>>>>>>>>DEBUG<<<<<<<<<<<<<<<<<<<')
+    db = firestore.client()
+    
+    user_id = event.params['user_id']
+    story_id = event.params['story_id']
+    
+    snapshot = event.data
+    story_data = snapshot.to_dict()
+    
+    story_idea = story_data['story_idea']
+    hero_name = story_data['hero_name']
+    language = story_data['language'].lower()
+    
     story_with_images_desc = generate_story_with_images_desc(language, story_idea, hero_name)
-    db_fn.reference(f"/Users/{user_id}/Stories/{story_id}/story_with_images_desc").set(story_with_images_desc)
-    return {"status": "ok"}
+    
+    story_with_images_links = add_dummy_image_to_story(story_with_images_desc)
+    
+    db.collection("Users").document(user_id).collection("Stories").document(story_id) \
+        .update(dict(story=story_with_images_links))
